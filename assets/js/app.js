a750d369f511db89d33a6d4581ccb1c0a88e06d9
Jeffeos <geoffrey.delenne@gmail.com>
2020-07-22 11:54:46 +0200

    added navbar and bootstrap

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,4 +11,15 @@ import '../css/app.scss';
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
 
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+const $ = require('jquery');
+// this "modifies" the jquery module: adding behavior to it
+// the bootstrap module doesn't export/return anything
+require('bootstrap');
+
+// or you can include specific pieces
+// require('bootstrap/js/dist/tooltip');
+// require('bootstrap/js/dist/popover');
+
+$(document).ready(function() {
+    $('[data-toggle="popover"]').popover();
+});
