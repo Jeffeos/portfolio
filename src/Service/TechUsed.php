ee7a5e460c0c8fa2cdbc29d59c86b7b313ebfd66
Jeffeos <geoffrey.delenne@gmail.com>
2020-07-23 15:51:53 +0200

    % use of techs is displayed on home page

--- src/Service/TechUsed.php
+++ src/Service/TechUsed.php
@@ -11,17 +11,16 @@ class TechUsed extends AbstractController
     public function percentUse(TechRepository $techRepository, ProjectRepository $projectRepository): array
     {
         $techs = $techRepository->findAll();
-        // dump($techs);
         $projectsTotal = count($projectRepository->findAll());
 
-        // Add each tech as key and %  of use as value
+        // Add each tech as key and % of use as value
         $percentUse = [];
         // Calculate the percentage of use of each tech I used: X projects out of X
         foreach ($techs as $tech)
         {
             $nbProjectsUsingTech = count($tech->getProjects());
             $percent = round(($nbProjectsUsingTech / $projectsTotal * 100), 0);
-            $percentUse[$tech->getName()] = $percent;
+            $percentUse[$tech->getId()] = $percent;
         }
         dump($percentUse);
 
